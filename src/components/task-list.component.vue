48a8dd0feb22941095027fa878fd0e8ea6ad1584
ClauRozasG <u201922910@upc.edu.pe>
2022-09-25 14:41:07 -0500

    Initial commit

---
+++ src/components/task-list.component.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>Tasky</h2>
+    <h3>Task Management</h3>
+    <ul>
+      <li v-for="task in tasks"
+        v-bind:key="task.id">
+        <task-item v-bind:task="task"
+        v-on:delete-task="$emit('delete-task', task.id)"></task-item> <!--lo escala a un nivel superior -->
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import TaskItem from "@/components/task-item.component.vue";
+export default {
+  name: "task-list",
+  components: {TaskItem},
+
+  props: [
+      "tasks"
+  ]
+}
+</script>
+
+<style scoped>
+
+</style>
